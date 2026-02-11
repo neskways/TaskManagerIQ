3a06ae5935e9bf0cd3bd1244f71274848fbc553b
neskways <timka77745tim@gmail.com>
2026-02-11 20:08:04 +0300

    misha

--- src/components/Surprise/Surprise.jsx
+++ src/components/Surprise/Surprise.jsx
@@ -1,19 +1,19 @@
 import s from "./Surprise.module.scss";
 import Cookies from "js-cookie";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export const Surprise = () => {
   const userCode = Cookies.get("userCode");
-
+  const [show, setShow] = useState(false)
   // Словарь: userCode → путь к звуку
   const userSounds = {
-    "000000002": "/sounds/podarok.mp3",      // Тимур
-    "000000063": "/sounds/podarok.mp3",      // Роман
-    "000000071": "/sounds/podarok.mp3",    // Айдамир
-    "000000016": "/sounds/podarok.mp3",  // Александр
-    "000000003": "/sounds/podarok.mp3",   // Виктория
-    "000000054": "/sounds/podarok.mp3",     // Кирилл
-    "000000007": "/sounds/podarok.mp3",   // Роман 1С
+    "000000063": "/sounds/podarok.mp3", // Роман
+    "000000071": "/sounds/porno.mp3",   // Айдамир
+    "000000016": "/sounds/slava.mp3",   // Александр
+    "000000017": "/sounds/ston.mp3",   // Александр
+    "000000003": "/sounds/rebetya.mp3", // Виктория
+    "000000054": "/sounds/ston.mp3",    // Кирилл
+    "000000007": "/sounds/porno.mp3",   // Роман 1С
   };
 
   const soundPath = userSounds[userCode];
@@ -26,16 +26,24 @@ export const Surprise = () => {
     audio.play().catch((err) => {
       console.error("Ошибка при воспроизведении звука:", err);
     });
+
+    if(userCode === "000000063") {
+      setShow(true);
+    }
+
   }, [soundPath]);
 
   return (
-    <img
-      className={s.podarok}
-      src="/images/podarok.png"
-      alt=""
-      title="ЖМИ! ЖМИ!"
-      onClick={playUserSound}
-      style={{ cursor: "pointer" }}
-    />
+    <>
+      <img className={`${s.podarok2} ${show ? s.show : ""}`} src="/images/memes/makaka.gif" alt="" />
+      <img
+        className={s.podarok}
+        src="/images/podarok.png"
+        alt=""
+        title="ЖМИ! ЖМИ!"
+        onClick={playUserSound}
+        style={{ cursor: "pointer" }}
+      />
+    </>
   );
 };
